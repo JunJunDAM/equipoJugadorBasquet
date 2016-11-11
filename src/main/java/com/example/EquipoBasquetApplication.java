8ac6f2019b6f7721d9329d9e56536282ac94093e
Jun <juninfo1997@gmail.com>
2016-11-11 17:58:44 +0100

    hola

--- src/main/java/com/example/EquipoBasquetApplication.java
+++ src/main/java/com/example/EquipoBasquetApplication.java
@@ -10,16 +10,20 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+@EntityScan(
+		basePackageClasses = {EquipoBasquetApplication.class, Jsr310JpaConverters.class}
+)
+
 @SpringBootApplication
 public class EquipoBasquetApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EquipoBasquetApplication.class, args);
 		ConfigurableApplicationContext context= SpringApplication.run(EquipoBasquetApplication.class, args);
 
 		context.getBean(EquipoService.class).equipos();
 		context.getBean(JugadorService.class).jugadores();
-
 
 	}
 }
